59779c63ecdaf866be84d46d437517a099cbfe7b
Markus <markus_andersson8@yahoo.se>
2017-06-27 12:39:13 +0200

    Added angularjs. Added start page with form. Added login page for admin and future registration.

---
+++ WebAPI_Pure/App/js/app.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="C:\Users\deltagare\Source\Repos\StrokingPenguin\WebAPI_Pure\Scripts/angular.js" />
+/// <reference path="C:\Users\deltagare\Source\Repos\StrokingPenguin\WebAPI_Pure\Scripts/angular-route.js" />
+
+
+(function () {
+    "use strict";
+
+    angular.module("app", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when("/", {
+            templateUrl: "App/html/startPage.html",
+            controller: "startCtrl",
+            controllerAs: "vm"
+        })
+        .when("/Login", {
+            templateUrl: "App/html/startPage.html",
+            controller: "adminLoginCtrl",
+            controllerAs: "vm"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+    });
+
+})();
---
+++ WebAPI_Pure/App/js/startController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app")
+    .controller("startCtrl", function () {
+        var vm = this;
+
+        vm.show = true;
+        vm.phaseOneHide = true;
+    })
+    .controller("adminLoginCtrl", function () {
+        var vm = this;
+
+        vm.show = false;
+        vm.phaseOneHide = false;
+    });
+
+})();
