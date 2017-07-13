160410d162e7c7e4c0547a9501d546808beff68e
p <oxnardmontalvo@live.se>
2017-07-13 14:09:18 +0200

    Added change password to UserController. Cleanup. Shorten Token lifetime to 12 hours.

--- WebAPI_Pure/App_Start/Startup.Auth.cs
+++ WebAPI_Pure/App_Start/Startup.Auth.cs
@@ -37,8 +37,8 @@ namespace WebAPI_Pure
             {
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                //AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(12),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
