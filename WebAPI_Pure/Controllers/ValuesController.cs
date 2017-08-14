6cf6db726df33fbe988919f80dd6b53107d0ed60
p <oxnardmontalvo@live.se>
2017-08-14 10:34:47 +0200

    Added put in UserFlyersController to save changes for only one cat.

--- WebAPI_Pure/Controllers/ValuesController.cs
+++ WebAPI_Pure/Controllers/ValuesController.cs
@@ -573,6 +573,41 @@ namespace WebAPI_Pure.Controllers {
 				return InternalServerError();
 			}
 		}
+		// id is catID
+		[Route("api/UserFlyers/{id}")]
+		public async Task<IHttpActionResult> Put(int id, [FromBody]HashSet<UserFlyersViewModel> vm) {
+			try {
+				var guid = User.Identity.GetUserId();
+				if ( guid == null ) {
+					return NotFound();
+				}
+
+				var user = await DB.Users.Include(x => x.Flyers).FirstOrDefaultAsync(x => x.Id == guid);
+				var flyers = new HashSet<Flyer>(DB.Flyers.Where(x => x.Category.ID == id && x.Category.Active == true && x.Active == true && x.Category.Flyers.Count > 0));
+
+				if ( user == null || flyers == null ) {
+					return NotFound();
+				}
+
+				flyers.RemoveWhere(x => vm.FirstOrDefault(y => y.ID == x.ID && x.Category.ID == id).Selected != true);
+				user.Flyers.RemoveWhere(x => vm.FirstOrDefault(y => y.ID == x.ID && x.Category.ID == id).Selected != true);
+
+				foreach ( var f in flyers.Where(x => x.Category.ID == id) ) {
+					if ( !user.Flyers.Contains(f) ) {
+						user.Flyers.Add(f);
+					}
+				}
+
+				var result = await DB.SaveChangesAsync();
+
+				if ( result == 0 ) {
+					return Conflict();
+				}
+				return Ok(user.Flyers);
+			} catch {
+				return InternalServerError();
+			}
+		}
 	}
 
 
