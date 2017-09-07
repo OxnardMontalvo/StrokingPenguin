af167623dcc5d98d4a647ce6680c16dab20100b2
Markus <markus_andersson8@yahoo.se>
2017-09-07 10:24:13 +0200

    Fixed print view, fixed user page not select all. Fixed loggin not to show regex.

--- WebAPI_Pure/sadhjgHFDXhghIYFVUrfgtjhpoPOIHUONerfgobui/userCtrl.js
+++ WebAPI_Pure/sadhjgHFDXhghIYFVUrfgtjhpoPOIHUONerfgobui/userCtrl.js
@@ -5,22 +5,16 @@
         // The start controller for displaying the info and getting the user information.
         .controller("userCtrl", function (user, userInfo) {
             var vm = this;
-
+            
             vm.cats = [];
             userInfo.get(function (data) {
-                //console.log(data);
                 angular.copy(data, vm.cats);
             });
-
-            console.log(vm.cats);
-
-            vm.allFlyers = [];
-            vm.curCatFlyers = [];
+            
             vm.checkChange = function (cat) {
                 console.log(cat.Flyers);
 
                 userInfo.savePut({ id: cat.ID }, cat.Flyers, function (response) {
-                    consol.log(cat.ID);
                     console.log(response);
                 });
             };
